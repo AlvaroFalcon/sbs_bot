5fda5377cf6de14674b60d35b5985f54da4eb536
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2023-09-06 15:01:02 +0100

    feat: added more commands

--- command_manager/command_manager.py
+++ command_manager/command_manager.py
@@ -1,11 +1,61 @@
 from . import waifu_manager
 
-commands = ["!waifu", "!commands"]
+commands = ["!waifu", "!commands","!neko","!shinobu","!megumin","!bully","!cuddle","!cry","!hug","!awoo","!kiss","!lick","!pat","!smug","!bonk","!yeet","!blush","!smile","!wave","!highfive","!handhold","!kick","!kill","!happy","!wink","!dance","!cringe"]
 custom_commands = []
 
 def manage_command(command, splittedMessage, context, update):
     if command == "!waifu":
         waifu_manager.get_waifu_image(context, update)
+    if command == "!neko":
+        waifu_manager.get_neko(context, update)
+    if command == "!shinobu":
+      waifu_manager.get_shinobu(context, update)
+    if command == "!megumin":
+      waifu_manager.get_megumin(context, update)
+    if command == "!bully":
+      waifu_manager.get_bully(context, update)
+    if command == "!cuddle":
+      waifu_manager.get_cuddle(context, update)
+    if command == "!cry":
+      waifu_manager.get_cry(context, update)
+    if command == "!hug":
+      waifu_manager.get_hug(context, update)
+    if command == "!awoo":
+      waifu_manager.get_awoo(context, update)
+    if command == "!kiss":
+      waifu_manager.get_kiss(context, update)
+    if command == "!lick":
+      waifu_manager.get_lick(context, update)
+    if command == "!pat":
+      waifu_manager.get_pat(context, update)
+    if command == "!smug":
+      waifu_manager.get_smug(context, update)
+    if command == "!bonk":
+      waifu_manager.get_bonk(context, update)
+    if command == "!yeet":
+      waifu_manager.get_yeet(context, update)
+    if command == "!blush":
+      waifu_manager.get_blush(context, update)
+    if command == "!smile":
+      waifu_manager.get_smile(context, update)
+    if command == "!wave":
+      waifu_manager.get_wave(context, update)
+    if command == "!highfive":
+      waifu_manager.get_highfive(context, update)
+    if command == "!handhold":
+      waifu_manager.get_handhold(context, update)
+    if command == "!kick":
+      waifu_manager.get_kick(context, update)
+    if command == "!kill":
+      waifu_manager.get_kill(context, update)
+    if command == "!happy":
+      waifu_manager.get_happy(context, update)
+    if command == "!wink":
+      waifu_manager.get_wink(context, update)
+    if command == "!dance":
+      waifu_manager.get_dance(context, update)
+    if command == "!cringe":
+      waifu_manager.get_cringe(context, update)      
     if command == "!commands":
         available_commmands(context, update)
     return
@@ -17,5 +67,5 @@ def is_command(text: str):
     return command in commands
 
 def available_commmands(context, update):
-    result = " ".join(line.strip() for line in commands)
+    result = "\n".join(line.strip() for line in commands)
     context.bot.send_message(chat_id=update.effective_chat.id, text=str("Comandos disponibles: "+result))
--- command_manager/waifu_manager.py
+++ command_manager/waifu_manager.py
@@ -1,5 +1,87 @@
 import requests
 
 def get_waifu_image(context, update):
-    response = requests.get("https://api.waifu.pics/sfw/waifu")    
-    context.bot.send_photo(chat_id=update.effective_chat.id, photo=str(response.json()["url"]))
+    response = requests.get("https://api.waifu.pics/sfw/waifu")
+    send_to_channel(context, update, str(response.json()["url"]))	
+def get_neko(context, update):
+    response = requests.get("https://api.waifu.pics/sfw/neko") 
+    send_to_channel(context, update, str(response.json()["url"]))	
+def get_shinobu(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/shinobu") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_megumin(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/megumin") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_bully(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/bully") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_cuddle(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/cuddle") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_cry(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/cry") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_hug(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/hug") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_awoo(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/awoo") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_kiss(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/kiss") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_lick(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/lick") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_pat(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/pat") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_smug(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/smug") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_bonk(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/bonk") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_yeet(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/yeet") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_blush(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/blush") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_smile(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/smile") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_wave(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/wave") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_highfive(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/highfive") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_handhold(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/handhold") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_kick(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/kick") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_kill(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/kill") 
+	send_to_channel(context, update, str(response.json()["url"]))		
+def get_happy(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/happy") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_wink(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/wink") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_dance(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/dance") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+def get_cringe(context, update):
+	response = requests.get("https://api.waifu.pics/sfw/cringe") 
+	send_to_channel(context, update, str(response.json()["url"]))	
+
+def send_to_channel(context, update, document):
+	print(document)
+	if ".gif" in document:
+		context.bot.sendDocument(chat_id=update.effective_chat.id, document = document)
+	else:
+		context.bot.send_photo(chat_id=update.effective_chat.id, photo=document)
